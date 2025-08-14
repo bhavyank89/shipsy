015610e8d61fe42d00e61c1d988df39951f1679d
bhavyank89 <bhavyanksarolia@gmail.com>
2025-08-14 14:07:33 +0530

    fixed auth.js middleware

--- apps/backend/middleware/auth.js
+++ apps/backend/middleware/auth.js
@@ -4,12 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default function auth(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    const token = req.headers.authorization;
+    if (!token) {
         return res.status(401).json({ message: "Unauthorized: Missing token" });
     }
-
-    const token = authHeader.split(" ")[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
